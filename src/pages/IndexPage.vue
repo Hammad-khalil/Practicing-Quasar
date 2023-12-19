7cbd5c67846bf862969df257cb54b7152105056b
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-19 23:41:27 +0500

    By using Methods, In this code by click on counter button it is calling messageUppercase too

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,16 +2,21 @@
 <!-- eslint-disable eqeqeq -->
 <template>
   <q-page padding>
+
+  <button @click="count++">{{count}}</button>
   <input v-model="message"
   @keyup="handlekeyup"
   @keyup.enter="enterMethod"
   >
-  // By using V-show it will make display none when condition does not match and vice versa
+   <!-- By using V-show it will make display none when condition does not match and vice versa -->
   <h5
    v-if="message.length>0" class="border-gray"
   >{{message}}</h5>
   <h6 v-else> No Message Left 😐</h6>
   <button @click="emptyMessage">Vanish input data</button>
+  <hr>
+
+  <h6>{{messageUppercase()}}</h6>
   </q-page>
 </template>
 
@@ -19,7 +24,9 @@
 export default {
   data () {
     return {
-      message: 'We Can Bind Data of input bar and Data property value'
+      message: 'Enter any text',
+      count: 0
+      // message: 'We Can Bind Data of input bar and Data property value'
     }
   },
   methods: {
@@ -34,7 +41,12 @@ export default {
         console.log(e.keyCode)
       }
     },
-    enterMethod () { alert(this.message) }
+    enterMethod () { alert(this.message) },
+    messageUppercase () {
+      console.log('Message Uppercase Button was fired !!!')
+      return this.message.toUpperCase()
+    }
+
   }
 }
 </script>
