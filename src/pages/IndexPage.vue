e52dc9cae8d4268a8e2e499f93a3d96c785b11b9
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-20 00:48:52 +0500

    Basics Completed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,16 @@
 <!-- eslint-disable eqeqeq -->
 <!-- eslint-disable eqeqeq -->
 <template>
-  <q-page padding>
 
+ <q-knob
+      v-model="value"
+      size="45px"
+      :thickness="1"
+      color="grey-8"
+      track-color="light-blue"
+      class="q-ma-md"
+    />
+  <q-page padding>
   <button class="btn" @click="count++">{{count}}</button>
   <input v-model="message"
   @keyup="handlekeyup"
@@ -20,7 +28,35 @@
   <hr>
 
   <h6>{{messageUppercase}}</h6>
+
+  <div class="q-pa-md">
+    <q-parallax>
+      <template v-slot:media>
+        <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      </template>
+
+      <template v-slot:content="scope">
+        <div
+          class="absolute column items-center"
+          :style="{
+            opacity: 0.45 + (1 - scope.percentScrolled) * 0.55,
+            top: (scope.percentScrolled * 60) + '%',
+            left: 0,
+            right: 0
+          }"
+        >
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" style="width: 150px; height: 150px">
+          <div class="text-h3 text-white text-center">Quasar Framework</div>
+          <div class="text-h6 text-grey-3 text-center">
+            v{{ $q.version }}
+          </div>
+        </div>
+      </template>
+    </q-parallax>
+  </div>
+
   </q-page>
+
 </template>
 
 <script>
